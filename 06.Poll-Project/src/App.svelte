e0ec0a3cd43d5948bd0fa4f4fe9bcd06aea44977
Daniël R <danielroth1890@gmail.com>
2021-03-24 02:44:45 +0100

    add header and footer, add poll buttons.

---
+++ 06.Poll-Project/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+</script>
+
+<Header />
+<main>
+	<!-- Op deze manier pass je data als properties mee aan een component. -->
+	<Tabs {activeItem} {items} />
+</main>
+<Footer />
+
+
+<style>
+	main {
+		max-width: 75rem;
+		margin: 40px auto;
+	}
+</style>
---
+++ 06.Poll-Project/src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <div class="copyright">Copyright 2021 | Daniël Coding</div>
+</footer>
+
+<style>
+    footer {
+        padding: 40px;
+        text-align: center;
+    }
+
+    .copyright {
+        color: #666;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #aaa;
+    }
+</style>
